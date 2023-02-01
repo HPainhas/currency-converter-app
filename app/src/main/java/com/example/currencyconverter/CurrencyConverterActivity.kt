be40e76ed523b438daa715d936bc2f5dca38689e
henrique-painhas <hpainhas@gmail.com>
2023-01-31 19:13:13 -0700

    handled navigation bar listeners

--- app/src/main/java/com/example/currencyconverter/CurrencyConverterActivity.kt
+++ app/src/main/java/com/example/currencyconverter/CurrencyConverterActivity.kt
@@ -3,8 +3,7 @@ package com.example.currencyconverter
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class CurrencyConverterActivity : AppCompatActivity() {
-
+class CurrencyConverterActivity : AppCompatActivity(R.layout.activity_currency_converter) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
--- navigation-bar/src/main/java/com/example/currencyconverter/navigationbar/NavigationBarFragment.kt
+++ navigation-bar/src/main/java/com/example/currencyconverter/navigationbar/NavigationBarFragment.kt
@@ -1,7 +1,65 @@
 package com.example.currencyconverter.navigationbar
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.currencyconverter.navigationbar.databinding.NavigationBarFragmentBinding
 
 class NavigationBarFragment : Fragment(R.layout.navigation_bar_fragment) {
 
+    private lateinit var binding: NavigationBarFragmentBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = NavigationBarFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.navigationBar.setOnItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.navigation_bar_menu_convert -> {
+                    // handle navigation to convert screen
+                    true
+                }
+                R.id.navigation_bar_menu_chart -> {
+                    // handle navigation to chart screen
+                    true
+                }
+                R.id.navigation_bar_menu_home -> {
+                    // handle navigation to home screen
+                    true
+                }
+                R.id.navigation_bar_menu_favorites -> {
+                    // handle navigation to favorites screen
+                    true
+                }
+                else -> false
+            }
+        }
+
+        binding.navigationBar.setOnItemReselectedListener { item ->
+            when(item.itemId) {
+                R.id.navigation_bar_menu_convert -> {
+                    // handle navigation to convert screen
+                }
+                R.id.navigation_bar_menu_chart -> {
+                    // handle navigation to chart screen
+                }
+                R.id.navigation_bar_menu_home -> {
+                    // handle navigation to home screen
+                }
+                R.id.navigation_bar_menu_favorites -> {
+                    // handle navigation to favorites screen
+                }
+            }
+        }
+    }
 }
